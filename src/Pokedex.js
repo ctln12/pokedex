ed3edadb238620d7b8984b9fa6c4bc2861272fd8
Cataline Baert <snuf_kin@yahoo.com>
2020-10-04 14:31:51 +0200

    add two new props to pokedex component

--- src/Pokedex.js
+++ src/Pokedex.js
@@ -5,10 +5,12 @@ import './Pokedex.css';
 
 class Pokedex extends Component {
   render() {
-    const {pokemons} = this.props;
+    const {pokemons, exp, isWinner} = this.props;
     return (
       <div className="Pokedex">
         <h1 className="Pokedex-title">Pokedex</h1>
+        <p>Total Experience: {exp}</p>
+        <p>{isWinner ? 'WINNER' : 'LOSER'}</p>
         <div className="Pokedex-pokemons">
           {pokemons.map(pokemon => <Pokecard key={pokemon.id} pokemon={pokemon} />)}
         </div>
