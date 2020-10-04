c510da10633734ccf7924ba53279962bcf5ce850
Cataline Baert <snuf_kin@yahoo.com>
2020-10-04 13:46:02 +0200

    change image source and create new function

--- src/Pokecard.js
+++ src/Pokecard.js
@@ -2,12 +2,15 @@ import React, { Component } from 'react';
 
 import './Pokecard.css';
 
-const POKE_API = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/";
+// const POKE_API = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/";
+const POKE_API = "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/";
+
+let padToThree = (number) => (number <= 999 ? `00${number}`.slice(-3) : number);
 
 class Pokecard extends Component {
   render() {
     let {pokemon} = this.props;
-    let imgUrl = `${POKE_API}${pokemon.id}.png`;
+    let imgUrl = `${POKE_API}${padToThree(pokemon.id)}.png`;
     return (
       <div className="Pokecard">
         <h3>{pokemon.name}</h3>
