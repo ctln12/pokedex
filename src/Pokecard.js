e8443386a4ac0fe34c61ebd93a49a6d238f1bec5
Cataline Baert <snuf_kin@yahoo.com>
2020-10-04 13:28:34 +0200

    refactor image url by creating a constant

--- src/Pokecard.js
+++ src/Pokecard.js
@@ -2,10 +2,12 @@ import React, { Component } from 'react';
 
 import './Pokecard.css';
 
+const POKE_API = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/";
+
 class Pokecard extends Component {
   render() {
     let {pokemon} = this.props;
-    let imgUrl = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}.png`;
+    let imgUrl = `${POKE_API}${pokemon.id}.png`;
     return (
       <div className="Pokecard">
         <h3>{pokemon.name}</h3>
